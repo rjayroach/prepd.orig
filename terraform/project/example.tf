4d7968ba6f4d43d5a542b5203a68bd51f028a458
Robert Roach <rjayroach@gmail.com>
2016-12-22 17:54:54 +0800

    small cleanup

--- terraform/project/example.tf
+++ terraform/project/example.tf
@@ -8,16 +8,30 @@ provider "aws" {
 
 # Debian Jessie
 variable "amis" {
-  type = "map"
+  type    = "map"
   default = {
-    us-east-1 = "ami-c8bda8a2"
+    us-east-1      = "ami-c8bda8a2"
     ap-southeast-1 = "ami-73974210"
   }
 }
 
+# Route53
+resource "aws_route53_zone" "primary" {
+  name    = "primary-example.com."
+  comment = "HostedZone created by Route53 Registrar and Managed by Terraform"
+}
+
+resource "aws_route53_record" "gmail_txt" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "${aws_route53_zone.primary.name}"
+  type    = "TXT"
+  ttl     = "3600"
+  records = ["google-site-verification=text verification code from google"]
+}
+
 resource "aws_key_pair" "ansible" {
-  key_name = "ansible"
-  public_key = "${file(../../.id_rsa.pub)}"
+  key_name   = "ansible"
+  public_key = "${file("../../.id_rsa.pub")}"
 }
 
 resource "aws_instance" "example" {
@@ -25,10 +39,9 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   key_name = "ansible"
   tags {
-    Application = "development"
     Environment = "staging"
-    Name = "rails-1"
-    Role = "app-server"
+    Name        = "rails-1"
+    Role        = "cluster-node"
   }
 }
 
