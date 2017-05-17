c9555470fa820be8ca064fcceea93974ddb692bb
Robert Roach <rjayroach@gmail.com>
2017-05-17 09:21:51 +0800

    remove template function in favor of straight-up repo

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing Ansible..."
-apt-get install -y apt-transport-https libssl-dev libffi-dev python-dev build-essential python-setuptools
-easy_install pip
-pip install -U setuptools cryptography markupsafe
-pip install -U ansible boto
